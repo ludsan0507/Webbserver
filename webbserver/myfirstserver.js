7b7d7c0f3f42b3c3cbfd50827728ab389d3386d7
ludsan0507 <ludsan0507@gmail.com>
2020-09-15 09:57:45 +0200

    dagens jobb

---
+++ webbserver/myfirstserver.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const clientDir = __dirname + "\\client\\"
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.get('/', (req, res) => res.sendFile(clientDir + "myfirstserver.html"))
+
+app.get('/myfirstserver.css', (req, res) => {
+    res.sendFile(clientDir + 'myfirstserver')
+})
+
+app.get('/kingkong.png', (req, res) => {
+    res.sendFile(clientDir + 'kingkong.png')
+})
+
+app.post('/', (req,res) => {
+    console.log(req.body.name)
+    console.log(req.body.email)
+    res.redirect('/')
+})
+
