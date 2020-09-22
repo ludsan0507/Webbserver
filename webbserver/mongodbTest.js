c75016449dbdc255e0e8803459cb2c5e549a61d7
ludsan0507 <ludsan0507@gmail.com>
2020-09-22 10:51:20 +0200

    09-22

--- webbserver/mongodbTest.js
+++ webbserver/mongodbTest.js
@@ -1,35 +1,58 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+  
 });
-const kittySchema = new mongoose.Schema({
-    name: String
+
+  const kittySchema = new mongoose.Schema({
+    name: String,
+    email: String,
   });
+  
   const Kitten = mongoose.model('Kitten', kittySchema);
+  
   const silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
-kittySchema.methods.speak = function () {
+
+  console.log(silence.name); // 'Silence'
+
+  
+  const personSchema = new mongoose.Schema({
+    name: String,
+    age:Number
+  });
+
+  personSchema.method.speak = () => {
+    console.log("hej");
+    const presentation = this.name 
+    ? `my name is ${this.name}` 
+    :`we are watching you!`;
+    console.log(presentation);
+  }
+  
+  kittySchema.methods.speak = function () {
     const greeting = this.name
       ? "Meow name is " + this.name
       : "I don't have a name";
     console.log(greeting);
   }
-  
-  const Kitten = mongoose.model('Kitten', kittySchema);
-  const fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak(); // "Meow name is fluffy"
 
-fluffy.save(function (err, fluffy) {
+  const Person= mongoose.model('Person', personSchema);
+  
+  const Ludvig = new Person({ name: "Ludvig", age: 18 });
+
+    Ludvig.speak(); 
+
+  Ludvig.save(function (err, Ludvig) {
     if (err) return console.error(err);
-    fluffy.speak();
+    Ludvig.speak();
   });
 
-  Kitten.find(function (err, kittens) {
-    if (err) return console.error(err);
-    console.log(kittens);
-  })
+  Ludvig.save()
 
-  Kitten.find({ name: /^fluff/ }, callback);
+ if(!Person.find({name: Ludvig.name }))
+  Ludvig.save()
+
+  Person.find({ name:"Ludvig"}, callback);
