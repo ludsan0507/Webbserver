a3087cbd32c4100896739e7a2641b2f126cfa058
ludsan0507 <ludsan0507@gmail.com>
2020-11-24 10:26:34 +0100

    11-24

--- webbserver/app.js
+++ webbserver/app.js
@@ -8,13 +8,18 @@ const store = require('./mongodbTest')
 const clientDir = __dirname + "\\client\\"
 
 app.use(express.json())
-app.use(express.json())
 app.use(express.urlencoded())
 
-app.get('/', (req, res) => res.sendFile(clientDir + "myfirstserver.html"))
+app.set('view engine', 'ejs')
 
-app.get('/myfirstserver.css', (req, res) => {
-    res.sendFile(clientDir + 'myfirstserver')
+app.get('/', (req, res) => {
+  res.render('pages/index.ejs', {name:"Ludde"})
+})
+
+app.get('/', (req, res) => res.sendFile(clientDir + "index.ejs"))
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(clientDir + 'index')
 })
 
 app.get('/postmalone.png', (req, res) => {
@@ -22,9 +27,11 @@ app.get('/postmalone.png', (req, res) => {
 })
 
 app.post('/', (req,res) => {
+
     let person = personmodul.createPerson(req.body.name, req.body.email, req.body.age)
     person.store();
     res.redirect('/')
 })
 
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
 
